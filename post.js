9b8ea013d0c8dd729d1cdd1071b4bcfd46d82246
Omolewu Sunday <omolewu12@gmail.com>
2022-08-28 12:12:55 +0100

    adding table

--- post.js
+++ post.js
@@ -5,13 +5,36 @@
 //     .then(data => {
 //         console.log(data)
 //     })
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((data) => {
-        let firstuser = data[0];
+        let table = `
+        <table><tr>
+        <th>S/N</th>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Phone</th>
+        <th>Email</th>
+        <th>Address</th>
+        </tr>
+        `;
+
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i].id)
+            table += `<tr>
+            <td> ${data[i].id} </td>
+            <td> ${data[i].name} </td>
+            <td> ${data[i].username} </td>
+            <td> ${data[i].phone} </td>
+            <td> ${data[i].email} </td>
+            <td> ${data[i].address.suite}
+                 ${data[i].address.street}
+                 ${data[i].address.city}
+           </td>
+           </tr>`
+
         }
-        console.log(firstuser)
+        table += "</table>";
+        document.getElementById('table').innerHTML = table;
     }
     )
